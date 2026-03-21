37e15e3b045f290a9d46e13d7ccc526fff65d1f6
Janice Cotcher <42756980+janice-cotcher@users.noreply.github.com>
2026-03-21 01:48:31 -0600

    update path code

--- paths.py
+++ paths.py
@@ -1,11 +1,22 @@
 from stl import mesh
 import numpy as np
+from path_utility import find_crease_edges, chain_crease_edges, coords_to_indices
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from matplotlib.path import Path
 
-vertices = []
+ls = []
 data = mesh.Mesh.from_file('mirrored.stl')
 vectors = data.vectors
-for triangles in vectors:
-    for point in triangles:
-        vertices.append(point)
 
-print(vertices[0])
+for triangle in vectors:
+    for point in triangle:
+        ls.append(point)
+
+vertices = np.array(ls)
+unique_vertices = np.unique(vertices, axis=0)
+triangles = coords_to_indices(unique_vertices, vectors)
+crease_edges = find_crease_edges(vertices, triangles, target_angle_deg=90.0, tolerance_deg=10)
+paths = chain_crease_edges(crease_edges)
+for path in paths:
+    coord = vertices[path]
